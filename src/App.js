5882f149cde0529b07149364a36a144ed10ed4e0
Miguel Perez <mirdarondon@gmail.com>
2025-10-21 15:49:22 -0400

    Responsive Design

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import photo from "./assets/img/photo.webp";
 
 function App() {
   return (
-    <div class="App">
+    <div class="bg-gradient-to-b from-slate-900 via-cyan-950 to-gray-900 min-h-screen min-w-full App">
       <header>
         <nav
           class="bg-gradient-to-r from-slate-900 via-gray-800 to-sky-950 h-20 border-b-8 border-b-slate-950 flex flex-row-reverse"
@@ -30,7 +30,7 @@ function App() {
                 src={linkedin_icon}
                 alt="Linkedin"
                 title="Linkedin"
-                class="size-8 mt-3 mr-5 grayscale hover:grayscale-0 transition-all duration-500"
+                class="size-6 sm:size-8 md:size-9 mr-4 invert brightness-50 hover:invert-0 hover:brightness-100 transition-all duration-500"
               />
             </a>
             <a
@@ -42,7 +42,7 @@ function App() {
                 src={gmail_icon}
                 alt="Gmail"
                 title="Gmail"
-                class="size-8 mt-3 mr-5 grayscale hover:grayscale-0 transition-all duration-500"
+                class="size-6 sm:size-8 md:size-9 mr-4 invert brightness-50 hover:invert-0 hover:brightness-100 transition-all duration-500"
               />
             </a>
             <a
@@ -54,7 +54,7 @@ function App() {
                 src={telegram_icon}
                 alt="Telegram"
                 title="Telegram"
-                class="size-8 mt-3 mr-5 grayscale hover:grayscale-0 transition-all duration-500"
+                class="size-6 sm:size-8 md:size-9 mr-4 invert brightness-50 hover:invert-0 hover:brightness-100 transition-all duration-500"
               />
             </a>
             <a
@@ -66,34 +66,33 @@ function App() {
                 src={github_icon}
                 alt="Github"
                 title="Github"
-                class="size-8 mt-3 mr-5 grayscale hover:grayscale-0 transition-all duration-500"
+                class="size-6 sm:size-8 md:size-9 mr-4 invert brightness-50 hover:invert-0 hover:brightness-100 transition-all duration-500"
               />
             </a>
             <img
               src={logo_icon}
               alt="Logo"
-              class="w-8 absolute left-3 top-2.5 mt-3.5 mr-5"
+              class="w-8 absolute left-2 mr-5"
             />
           </div>
-          <p class="font-bold font-serif text-2xl absolute top-0 left-4 text-white pt-6 pl-8">
+          <p class="font-bold font-serif text-2xl absolute top-5 left-4 text-white pl-8">
             PotOfCode
           </p>
-          <p class="text-white font-bold font-sans pt-7 pr-4">Contacto</p>
         </nav>
       </header>
 
-      <main class="bg-gradient-to-b from-slate-900 via-cyan-950 to-gray-900 min-h-screen p-8">
-        <section class="flex flex-col items-center min-h-96 mb-24">
-          <h1 class="font-bold text-[#FFE9AE] text-4xl pb-4">Portfolio</h1>
+      <main class="mx-8 sm:mx-16 md:mx-22">
+        <section class="flex flex-col items-center min- mb-24">
+          <h1 class="font-bold text-[#FFE9AE] text-4xl pt-4 pb-4">Portfolio</h1>
           <img
             src={profile}
             alt="Perfil"
             class="object-cover rounded-full size-40"
           />
-          <h2 class="font-bold text-[#FFE9AE] text-4xl mt-4">
+          <h2 class="font-bold text-center text-[#FFE9AE] text-4xl mt-4">
             Saludos, soy PotOfCode
           </h2>
-          <p class="text-white p-2 w-1/2 text-left mt-4 font-semibold text-xl">
+          <p class="text-white text-center mx-4 sm:text-left sm:mx-8 md:mx-16 lg:mx-24 p-2 text-left mt-4 font-semibold text-xl">
             Estudiante de Ingeniería de Sistemas y desarrollador web. Apasionado
             por el aprendizaje continuo y la creación de experiencias digitales.
           </p>
@@ -104,15 +103,15 @@ function App() {
                 <img 
                   src={proyects_icon} 
                   alt="Proyectos"
-                  class="w-12 pr-4"
+                  class="size-16 pr-4"
                 />
                 <h3 class="font-bold text-white text-4xl">
                   Proyectos
                 </h3>
             </div> 
           </div> 
-          <article class="grid grid-cols-2 gap-8 px-32">
-            <div class="bg-zinc-900/50 border-black/20 border-2 rounded-lg h-[410px]">
+          <article class="sm:grid grid-cols-2 grid-flow-row auto-cols-max gap-4">
+            <div class="bg-zinc-900/50 border-black/20 border-2 rounded-lg mb-4 p-4">
               <a 
               href="https://www.carfixve.app/"
               target="_blank"
@@ -130,14 +129,14 @@ function App() {
                   target="_blank"
                   rel="noopener"
                   >
-                    <img src={github_icon} alt="Github" class="size-10 hover:scale-125 duration-200" />
+                    <img src={github_icon} alt="Github" class="w-8 hover:scale-125 duration-200 mr-4" />
                   </a>
-                  <h4 class="ml-4 text-[#00BD13] text-center text-2xl font-bold">CarFix - E-commerce</h4>
+                  <h4 class="text-[#00BD13] text-center text-xl font-bold">CarFix - E-commerce</h4>
                 </div>
-            <p class="pt-4 px-4 text-white text-lg text-left font-semibold">Plataforma de venta de repuestos de vehículos de la organización Bufferring. <strong class="text-[#00BD13] font-semibold">Líder del equipo de Frontend</strong> durante su desarrollo.
+            <p class="mt-4 mx-4 text-white text-lg text-left font-semibold">Plataforma de venta de repuestos de vehículos de la organización Bufferring. <strong class="text-[#00BD13] font-semibold">Líder del equipo de Frontend</strong> durante su desarrollo.
             </p>
             </div>
-            <div class="bg-zinc-900/50 border-black/20 border-2 rounded-lg px-4 h-[410px]">
+            <div class="bg-zinc-900/50 border-black/20 border-2 rounded-lg mb-4 p-4">
               <a 
               href="https://ai-financial-advisor-zniz.onrender.com/registro"
               target="_blank"
@@ -155,14 +154,14 @@ function App() {
                   target="_blank"
                   rel="noopener"
                   >
-                    <img src={github_icon} alt="Github" class="size-12 hover:scale-125 duration-200" />
+                    <img src={github_icon} alt="Github" class="w-8 hover:scale-125 duration-200 mr-4" />
                   </a>
-                  <h4 class="ml-4 text-[#00BD13] text-left text-xl font-bold">AI Financial Advisor - Gestor de Finanzas</h4>
+                  <h4 class="text-[#00BD13] text-center text-lg font-bold">AI Financial Advisor - Gestor de Finanzas</h4>
                 </div>
             <p class="pt-4 px-4 text-white text-lg text-left font-semibold">App web que gestiona datos como ingresos o deudas, que incluye un <strong class="text-[#00BD13] font-semibold">ChatBot AI</strong>.
             </p>
             </div>
-            <div class="bg-zinc-900/50 border-black/20 border-2 col-span-2 rounded-lg flex flex-row h-[300px]">
+            <div class="bg-zinc-900/50 border-black/20 border-2 sm:col-span-2 rounded-lg sm:flex sm:flex-row p-4">
               <a 
               href="https://text-analyzer-hpij.onrender.com/"
               target="_blank"
@@ -171,19 +170,19 @@ function App() {
                 <img 
                 src={text} 
                 alt="Carfix" 
-                class="ml-4 mt-8 w-[600px] brightness-100 hover:brightness-50 hover:scale-105 duration-300 border-2 border-white/50 shadow-2xl"
+                class="mx-auto mt-8 w-[600px] brightness-100 hover:brightness-50 hover:scale-105 duration-300 border-2 border-white/50 shadow-2xl"
                 />
               </a>
-                <div class="flex flex-col mt-8 ml-12">
-                  <div class="flex flex-row items-center">
+                <div class="flex flex-col mt-8">
+                  <div class="flex flex-row justify-center items-center">
                     <a 
                   href="https://github.com/PotOfCode/text_analyzer"
                   target="_blank"
                   rel="noopener"
                   >
-                      <img src={github_icon} alt="Github" class="size-10 hover:scale-125 duration-200" />
+                      <img src={github_icon} alt="Github" class="w-8 hover:scale-125 duration-200 mr-4" />
                     </a>
-                    <h4 class="ml-4 text-[#00BD13] text-left text-xl font-bold">Analizador de Texto</h4>
+                    <h4 class="text-[#00BD13] text-center text-xl font-bold">Analizador de Texto</h4>
                   </div>
                   <p class="pt-4 mr-4 px-4 text-white text-lg text-left font-semibold">Aplicación web segura que permite <strong class="text-[#00BD13] font-semibold">analizar archivos de texto</strong> con estadísticas detalladas.</p>
                 </div>
@@ -191,15 +190,20 @@ function App() {
           </article>
         </aside>
         <aside>
-          <div class="flex flex-col items-center justify-left text-center mt-40">
+          <div class="flex flex-col items-center justify-center text-center mt-40">
             <div class="flex flex-row items-center justify-left">
               <img src={about_me} alt="Sobre mi" class="w-16 pr-4" />
               <h5 class="font-bold text-white text-4xl">Acerca de mi</h5>
             </div>
           </div>
-          <div class="mt-16">
-            <div class="flex flex-row ml-48 w-5/6 h-96 gap-x-36">
-              <p class="text-white text-left text-xl font-bold w-1/2">
+          <div class="mt-16 lg:mx-40">
+            <div class="sm:flex flex-row">
+              <img
+                src={photo}
+                alt="Foto"
+                class="float-left h-64 border-4 border-[#253D52] rounded-lg mr-4 mb-8"
+              />
+              <p class="text-white text-left text-lg sm:text-lg font-bold max-w-[600px] mb-8 mx-4">
                 Me llamo Miguel Pérez, un estudiante de ingeniería en Sistemas.
                 Programando aún sin saber que lo estaba haciendo, aprendiendo
                 de prueba y error y descubriendo lo interesante de este mundo.
@@ -207,11 +211,6 @@ function App() {
                 apuntando a mejorar y llegar a dominar por completo el
                 desarrollo web.</p>
               </p>
-              <img
-                src={photo}
-                alt="Foto"
-                class="h-64 border-4 border-[#253D52] rounded-lg"
-              />
             </div>
           </div>
         </aside>
