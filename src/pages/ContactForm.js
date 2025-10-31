1cda382f8da15fd0d6505de045c42bbce64c7634
Miguel Perez <mirdarondon@gmail.com>
2025-10-31 15:19:06 -0400

    react-hook-form

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import home_icon from "../assets/icons/home.svg";
 import telegram_icon from "../assets/icons/telegram-icon.svg";
 import github_icon from "../assets/icons/github-icon.svg";
 import linkedin_icon from "../assets/icons/linkedin-icon.svg";
 import logo_icon from "../assets/icons/logo.svg";
 
-const ContactForm = () => (
+export default function ContactForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data, e) => {
+    e.target.submit();
+  }
+
+  return (
     <div class="flex flex-col bg-gradient-to-b from-slate-900 via-cyan-950 to-gray-900 min-h-screen min-w-full App">
       <header>
         <nav
@@ -75,34 +82,74 @@ const ContactForm = () => (
 
       <main class="mx-8 sm:mx-16 md:mx-22 flex-grow">
         <section class="flex justify-center justify-items-center">
-          <form action="https://formsubmit.co/potofcode8@gmail.com" method="POST" class="bg-gray-900/60 p-8 rounded-lg shadow-lg border border-gray-600 mt-4 mb-4"> 
+          <form action="https://formsubmit.co/potofcode8@gmail.com" method="POST" onSubmit={handleSubmit(onSubmit)} class="bg-gray-900/60 p-8 rounded-lg shadow-lg border border-gray-600 mt-4 mb-4"> 
             <p class="text-white font-bold text-xl">Nombre</p>
             <input
               type="text"
               name="Nombre"
               placeholder="Jose Andres Da-Silva"
-              required
+              {...register("Nombre", { 
+                required: {
+                  value: true,
+                  message: "Rellene este campo."
+                },
+                minLength: {
+                  value: 2,
+                  message: "El nombre debe tener al menos 2 caracteres."
+                },
+                maxLength: {
+                  value: 50,
+                  message: "El nombre no puede exceder los 50 caracteres."
+              },
+                pattern: {
+                  value: /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/i,
+                  message: "El nombre solo puede contener letras y espacios."
+                },
+              })}
               class="block w-full sm:w-96 md:w-96 max-w-md p-3 mb-4 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <p class="text-white font-bold text-xl">Correo Electronico</p>
+            {errors.Nombre && <p class="text-red-500 mb-2">{errors.Nombre.message}</p>}
+            <p class="text-white font-bold text-xl">Correo Electrónico</p>
             <input
               type="email"
-              name="Correo Electronico"
+              name="CorreoElectronico"
               placeholder="josesilva@gmail.com"
-              required
+              {...register("CorreoElectronico", { 
+                required: {
+                  value: true,
+                  message: "El correo es obligatorio."
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Introduzca un correo electrónico válido."
+                },
+              })}
               class="block w-full max-w-md p-3 mb-4 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            {errors.CorreoElectronico && <p class="text-red-500 mb-2">{errors.CorreoElectronico.message}</p>}
             <p class="text-white font-bold text-xl">Mensaje</p>
             <textarea 
               name="Mensaje" 
-              required 
               title="Tu mensaje debe tener entre 20 y 500 caracteres." 
-              maxlength="500" 
-              minlength="20" 
+              {...register("Mensaje", { 
+                required: {
+                  value: true,
+                  message: "El mensaje es obligatorio."
+                },
+                minLength: {
+                  value: 20,
+                  message: "El mensaje debe tener al menos 20 caracteres."
+                },
+                maxLength: {
+                  value: 500,
+                  message: "El mensaje no puede exceder los 500 caracteres."
+                },
+              })}
               class="block w-full max-w-md h-96 p-3 mb-4 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 text-le resize-none"
               placeholder="Escribe tu mensaje aquí..."
             ></textarea>
-            <input type="hidden" name="_next" value="http://localhost:3000/Contact"/>
+            {errors.Mensaje && <p class="text-red-500 mb-2">{errors.Mensaje.message}</p>}
+            <input type="hidden" name="_next" value="https://potofcode.vercel.app/Contact"/>
             <input type="hidden" name="_captcha" value="false"/>
             <button
               type="submit"
@@ -118,5 +165,4 @@ const ContactForm = () => (
       </footer>
     </div>
   );
-
-export default ContactForm;
+}
